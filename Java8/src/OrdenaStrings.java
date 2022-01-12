cc813cef065353d9ed30af5f656bba8d5d647dd4
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2022-01-12 11:55:58 -0300

    Que venham os lambdas

--- Java8/src/OrdenaStrings.java
+++ Java8/src/OrdenaStrings.java
@@ -1,58 +1,23 @@
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class OrdenaStrings {
-
 	public static void main(String[] args) {
-
-		//Criando a lista de strings e adcionando itens
+		// Criando a lista de strings e adcionando itens
 		List<String> palavras = new ArrayList<String>();
 		palavras.add("alura online");
-		palavras.add("editora casad o código");
+		palavras.add("editora casa do código");
 		palavras.add("caelum");
 
-		//Instancianco a class ComparadorPorTamanho
-//		Comparator<String> comparator = new ComparadorPorTamanho();
-		
-		//Usando o Collections para ordenar as strings, forma antiga
-		//Collections.sort(palavras, comparator);
-		
-		palavras.sort(new ComparadorPorTamanho()); //JAVA 8
-		
+		//Substituindo os ifs pelo método compare da class Integer que faz a mesma função de devolver positivo, negativo ou zero
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+
 		System.out.println(palavras);
 
-		//Forma antiga de iterar a lista
-//		for (String p : palavras) {
-//			System.out.println(p);
-//		}
-		
-//		Consumer<String> consumidor = new ImprimenaNaLinha();
-		palavras.forEach(new ImprimenaNaLinha());
-		
-	}
-
-}
-
-//JAVA 8
-class ImprimenaNaLinha implements Consumer<String>{
-	@Override
-	public void accept(String s) {
-		System.out.println(s);
-	}
-}
-
-//Criando a class do comparador
-class ComparadorPorTamanho implements Comparator<String>{
-	@Override
-	public int compare(String s1, String s2) {
-		if(s1.length() < s2.length())
-			return -1;
-		
-		if(s1.length() > s2.length())
-			return 1;
-			
-		return 0;
+		// Passo 1 - Criando e instanciando uma class anonima e passando ela por parametro para o método forEach de palavras
+		// Passo 2 - Como o forEach já sabe que ele irá receber um consumer e sabe que este consumer só tem 1 método para 
+		// implementar, posso usar uma expresão lambda para deixar o código mais enxuto
+		// Expressão lambda - Como só tem 1 parametro não preciso informar o tipo e nem colocar os parenteses
+		palavras.forEach(s -> System.out.println(s));
 	}
 }
