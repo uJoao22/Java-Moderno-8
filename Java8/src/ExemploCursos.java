002c8bf7812a94bac16d1f27a8bfd2cbde0b7dc2
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2022-01-12 14:14:07 -0300

    Mais Streams, Collectors e APIs

--- Java8/src/ExemploCursos.java
+++ Java8/src/ExemploCursos.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 
 class Curso {
 	private String nome;
@@ -30,6 +34,14 @@ public class ExemploCursos {
 		
 		cursos.sort(Comparator.comparing(Curso::getAlunos));
 		
-		cursos.stream().filter(c -> c.getAlunos() >= 100).map(Curso::getAlunos).forEach(System.out::println);
+		OptionalDouble media  = cursos.stream().filter(c -> c.getAlunos() >= 100).mapToInt(Curso::getAlunos).sum();
+
+		//Cursos, me dê o fluxo de todos os seus cursos, filtre quem tem mais de 100 alunos, encontre um deles, se você encontrou
+		//um deles, imprima o nome dele, se não encontro não faça nada
+		cursos.stream().filter(c -> c.getAlunos() >= 100).findAny().ifPresent(c -> System.out.println(c.getNome()));
+		
+		cursos.stream().filter(c -> c.getAlunos() >= 100).collect(Collectors.toMap(
+				c -> c.getNome(),
+				c -> c.getAlunos())).forEach((nome, alunos) -> System.out.println(nome+" tem "+alunos));
 	}
 }
